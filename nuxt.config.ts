92903f319869d226d43089ce5045abe8370a5b5e
brendaferrari <brendafferrari@gmail.com>
2024-09-07 11:04:31 +0300

    fix: css and js not working in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     },
   },
   app: {
+    baseURL: '/chemdata/',
     pageTransition: { name: "page", mode: "out-in" },
     head: {
       link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
